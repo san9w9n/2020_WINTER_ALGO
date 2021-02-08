09c397b3c1b00fb0b862be42c2920a84bf0eeafe
san9w9n <san9w9n@naver.com>
2021-02-08 12:48:01 +0900

    math-algorithm

---
+++ 3009.py
@@ -0,0 +1,21 @@
+import sys
+arr = []
+for _ in range(3):
+    arr.append(list(map(int,sys.stdin.readline().split())))
+
+if arr[0][0]==arr[1][0]:
+    x = arr[2][0]
+elif arr[0][0]==arr[2][0]:
+    x = arr[1][0]
+else:
+    x = arr[0][0]
+
+
+if arr[0][1]==arr[1][1]:
+    y = arr[2][1]
+elif arr[0][1]==arr[2][1]:
+    y = arr[1][1]
+else:
+    y = arr[0][1]
+
+print("{} {}".format(x,y))
