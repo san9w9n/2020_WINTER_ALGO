9c5afc3211ee4dc74ca09450e1d02a993539483d
san9w9n <san9w9n@naver.com>
2021-02-17 14:40:24 +0900

    dynamic-programming

---
+++ 10844.py
@@ -0,0 +1,15 @@
+import sys
+n=int(sys.stdin.readline())
+dp=[[0 for x in range(10)] for y in range(n)]
+for i in range(1,10): dp[0][i]=1
+
+for i in range(1,n):
+    for j in range(10):
+        if j==0:
+            dp[i][0]=dp[i-1][1]
+        elif j==9:
+            dp[i][9]=dp[i-1][8]
+        else:
+            dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1]
+
+print(sum(dp[n-1])%1000000000)
