f5cd333a87aa9d7f4e2c507745f106e510c27527
san9w9n <san9w9n@naver.com>
2021-03-02 19:34:31 +0900

    greedy

---
+++ greedy.py
@@ -0,0 +1,10 @@
+import sys
+
+s=list(sys.stdin.readline().rstrip())
+result=int(s[0])
+
+for i in range(1,len(s)):
+    if result<=0 or s[i]=='0' or s[i]=='1': result+=int(s[i])
+    else: result*=int(s[i])
+
+print(result)
