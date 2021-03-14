2b7924599b028eb895f42c46c9d4e8224cc6f6c8
2018008877 <sw994812@hanyang.ac.kr>
2021-03-14 18:17:44 +0900

    sort

---
+++ 2437.py
@@ -0,0 +1,15 @@
+import sys
+n=int(sys.stdin.readline())
+weight=list(map(int,sys.stdin.readline().split()))
+weight.sort()
+
+target=1
+for i in weight:
+  if target<i:
+    break
+  target+=i
+
+print(target)
+
+
+
