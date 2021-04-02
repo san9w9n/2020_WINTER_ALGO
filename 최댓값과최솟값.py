6bd0fd9505243f34b40976b0724467f6f874a9d5
2018008877 <sw994812@hanyang.ac.kr>
2021-04-02 11:57:46 +0900

    sort

---
+++ 최댓값과최솟값.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    arr=list(s.split())
+    for i in range(len(arr)):
+        arr[i]=int(arr[i])
+    arr.sort()
+    answer+=str(arr[0])
+    answer+=" "
+    answer+=str(arr[-1])
+    return answer
